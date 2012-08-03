5eb74e4ef1c29d319bd0a4c8d072de9fc47ba704
Alex Lawson <cheswick13@hotmail.com>
2012-08-03 14:57:03 -0700

    working on server

--- pongserver.js
+++ pongserver.js
@@ -1,8 +1,8 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
-var p1 = '';
-var p2 = '';
+var running = true;
+var clients = [ ];
 
 var server = http.createServer(function(request, response) {
     // process HTTP request. Since we're writing just WebSockets server
@@ -19,6 +19,9 @@ wsServer = new WebSocketServer({
 wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
 
+    // we need to know client index to remove them on 'close' event
+    var index = clients.push(connection) - 1;
+
     // This is the most important callback for us, we'll handle
     // all messages from users here.
     connection.on('message', function(message) {
@@ -31,5 +34,41 @@ wsServer.on('request', function(request) {
 
     connection.on('close', function(connection) {
         // close user connection
+        
+        // remove user from the list of connected clients
+        clients.splice(index, 1);
     });
 });
+
+var p1x = 250;
+var p2x = 250;
+var p1vx = 0;
+var p2vx = 0;
+var ballx = 250;
+var bally = 250;
+var ballvx = 1;
+var ballvy = 1;
+
+// looped game logic
+function doupdate() {
+    
+    
+};
+
+//update positions
+function moveobjects() {
+    //paddles
+    
+    //ball
+    
+}
+
+//check for collision
+function testcollide() {
+    //paddles
+    
+    //ball
+}
+
+// start main game loop
+var gameloop = setInterval(doupdate(), 100);
